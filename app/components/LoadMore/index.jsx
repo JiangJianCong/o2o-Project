d5cff4e05cbcf62fd9a90c52cb6e68ff445cdff0
spacehehe <910455361@qq.com>
2018-05-19 22:29:08 +0800

    M-木偶组件里面的List更新，新增了page页码，新增isLoadingMore这个状态来判断是否加载更多

---
+++ app/components/LoadMore/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import './style.less'
+
+class LoadMore extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+    render() {
+        return (
+            <div className="load-more">
+
+                {
+                    this.props.isLoadingMore
+                    ? <span>加载中。。。</span>
+                    : <span onClick={this.loadMoreHandle.bind(this)}>加载更多</span>
+                }
+
+            </div>
+        )
+    }
+
+
+    loadMoreHandle(){
+        //执行传递过来的loadMoreDataFn
+        this.props.loadMoreFn()
+    }
+}
+
+export default LoadMore
--- app/containers/Home/subpage/List.jsx
+++ app/containers/Home/subpage/List.jsx
@@ -3,14 +3,18 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 import './style.less'
 import {getListData} from "../../../fetch/home/home";
 import ListComponent from '../../../components/List/'
+import LoadMore from '../../../components/LoadMore'
 
 class List extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state ={
-            data:[],
-            hasMore : false
+            data:[],//存储列表信息
+            hasMore : false, //后段返回的 是否还有下一页
+            isLoadingMore : false,//记录当前状态是'加载中'还是"点击加载更多"
+            page : 1 , //下一页的页码
+
         }
     }
     render() {
@@ -22,7 +26,12 @@ class List extends React.Component {
                     ? <ListComponent data={this.state.data}/>
                     : <div>Loading...</div>
                 }
-                {/*列表*/}
+
+                {
+                    this.state.hasMore
+                    ? <LoadMore isLoadingMore={this.state.isLoadingMore} loadMoreFn={this.loadMoreData.bind(this)} />
+                    : <div></div>
+                }
                 {/*loadmore*/}
             </div>
         )
@@ -40,6 +49,28 @@ class List extends React.Component {
         this.resultHandle(result)
     }
 
+    //加载更多
+    loadMoreData() {
+        //点击，记录状态
+        this.setState({
+            isLoadingMore:true
+
+        })
+
+        const cityName = this.props.cityName
+        const page = this.state.page    //下一页的页码
+        const result = getListData(cityName,page)
+        this.resultHandle(result)
+
+        //增加page 的计数
+        this.setState({
+            page : page + 1 ,
+            isLoadingMore:false
+
+        })
+
+    }
+
     //数据处理
     resultHandle(result) {
         result.then(res=>{
@@ -49,8 +80,10 @@ class List extends React.Component {
             const hasMore = json.hasMore
             const data = json.data
             this.setState ({
-                data : data,
-                hasMore : hasMore
+
+                hasMore : hasMore,
+                data: this.state.data.concat(data)
+
             })
 
         })
