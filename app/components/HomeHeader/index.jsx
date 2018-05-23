a17e501f77c33823b9bf4e1dc55be9bc77d4033b
spacehehe <910455361@qq.com>
2018-05-23 22:07:55 +0800

    M-在头部的组件中，在state新增pwd，这是用来获取input的内容的，也就是监控input标签的变化，然后保存在state里，使用的时候可以直接从state里面读取，这样就避免每次读数据都操作一次dom，并且在input标签里面加入 value = {this.state.pwd} 来显示input的内容。

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -8,6 +8,9 @@ class HoneHeader extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            pwd : ''
+        }
     }
 
     render() {
@@ -27,11 +30,42 @@ class HoneHeader extends React.Component {
                 <div className='home-header-middle'>
                     <div className="search-container">
                         <i className="icon-search"></i>
-                        <input type="text" placeholder='请输入关键字'/>
+                        <input
+                            className='search-value'
+                            type="text"
+                            placeholder='请输入关键字'
+                            onChange={this.changeHandle.bind(this)}
+                            onKeyUp={this.keyUpHandle.bind(this)}
+                            value={ this.state.pwd }
+
+                        />
                     </div>
                 </div>
             </div>
         )
+    }
+
+    /**
+     * 实时更新state并且获取input的值
+     * @param e
+     */
+    changeHandle(e) {
+        var val = e.target.value
+        this.setState({
+            pwd : val
+        })
+    }
+
+    /**
+     * 键盘上抬的时候判断是否回车搜索
+     * @param e
+     */
+    keyUpHandle(e) {
+        if (e.keyCode !== 13) {
+            return
+        }
+        hashHistory.push('/search/all/' + encodeURIComponent(this.state.pwd))
+
     }
 }
 
