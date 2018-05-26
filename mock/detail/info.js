2229092efcf6ce54a2853ffc497bc3171d807c0d
spacehehe <910455361@qq.com>
2018-05-26 23:30:40 +0800

    A - mock里面新增了2组数据 M - 在server里面模拟了2个api

---
+++ mock/detail/info.js
@@ -0,0 +1,8 @@
+module.exports = {
+    img: 'https://p1.meituan.net/deal/e0666f70bdec6362a2f0aa92a4360e6e118690.jpg.webp@180w_180h_1e_1c_1l_80q%7Cwatermark=0',
+    title: '天下第一锅',
+    star: 4,
+    price: '88',
+    subTitle: '重庆 & 四川 麻辣火锅',
+    desc: '营业时间 11:00 - 21:00 <br> 电话订购 11:00 - 19:00 <br> 网络订购 11:00 - 19:00'
+}
